208b99041c39b2ef10cc578021d8484f8372eb3a
toojuu <toojuu>
2020-11-24 13:02:57 +0000

    Initial commit

---
+++ src/test/java/com/example/websocket/WebsocketTestTest.java
@@ -0,0 +1,21 @@
+package com.example.websocket;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class WebsocketTestTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
